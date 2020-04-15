9313ffcda0db818be1ead262f48aa9758e13153e
Daniel J Feleppa <dfeleppa@gmail.com>
2020-04-15 21:15:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,19 +15,17 @@ class Pokemon
     db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)", name, type)
     
   end  
-  #     def save
-  #   if self.id
-  #     self.update
-  #   else
-  #     sql = <<-SQL
-  #       INSERT INTO students (name, grade)
-  #       VALUES (?,?)
-  #       SQL
+      def save
+
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+        SQL
     
-  #     DB[:conn].execute(sql, self.name, self.grade)
-  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  #   end
-  # end
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 
 
   def self.find(id_number, db)
